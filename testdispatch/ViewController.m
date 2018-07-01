54bb917978ac77caa79e18eb6ea2598a98fbedca
晏立 <fengwu2006@gmail.com>
2018-07-01 18:59:25 +0800

    save

--- testdispatch/ViewController.m
+++ testdispatch/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property(weak) IBOutlet UILabel *myLable;
+
 @end
 
 @implementation ViewController
@@ -23,7 +25,88 @@
   
   [super viewDidAppear:animated];
   
-  [self testSerialQueue];
+  [self testNotUpdateUiInMainQueue];
+}
+
+- (void)testMainThreadAndMainQueue {
+  
+  dispatch_queue_t lowQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+  
+  NSLog(@"start");
+  
+  dispatch_sync(lowQueue, ^{
+    
+    NSLog(@"主线程 %d", [NSThread isMainThread]);
+    
+    NSLog(@"process");
+  });
+  
+  NSLog(@"finish");
+}
+
+- (void)testNotUpdateUiInMainQueue {
+  
+  dispatch_queue_attr_t attr = dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_CONCURRENT, QOS_CLASS_USER_INTERACTIVE, 0);
+  
+  dispatch_queue_t queue = dispatch_queue_create("interactive_concurrent_queue", attr);
+  
+  dispatch_sync(queue, ^{
+    
+    [self.myLable setText:@"ABC"];
+  });
+}
+
+- (void)testTargetQueue {
+  
+  dispatch_queue_t basequeue = dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0);
+  
+  dispatch_queue_t queue0 = dispatch_queue_create("queue0", 0);
+  
+  dispatch_set_target_queue(queue0, basequeue);
+  
+  
+}
+
+- (void)doSomeTest {
+  
+  dispatch_queue_t mainQueue = dispatch_get_main_queue();
+  
+  dispatch_block_t block = ^{
+    
+    NSLog(@"main thread %d", [NSThread isMainThread]);
+  };
+           
+  dispatch_queue_t globalQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+  
+  dispatch_async(globalQueue, ^{
+    
+    dispatch_async(mainQueue, block);
+  });
+  
+  dispatch_main();
+  
+  NSLog(@"do something");
+}
+
+- (void)testSpecific {
+  
+  dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+  
+  const char *key = "my_queue_key";
+  
+  dispatch_queue_set_specific(queue, key, "my_queue_context", NULL);
+  
+  dispatch_async(queue, ^{
+    
+    const char *context = dispatch_get_specific(key);
+    
+    if (context) {
+      
+      printf("%s", context);
+    }
+
+    NSLog(@"ok");
+  });
 }
 
 - (void)testSerialQueue {
@@ -36,10 +119,12 @@
   
   dispatch_queue_t serialQueue = dispatch_queue_create("serial_queue", attr);
   
-  dispatch_async(mainQueue, ^{
+  dispatch_async(serialQueue, ^{
 
     NSLog(@"1");
   });
+  
+  dispatch_main();
 
 //  dispatch_async(serialQueue, ^{
 //
